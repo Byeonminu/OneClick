d7ea1652d020dc4b5f8347d481ced56dbc464539
Byeonminu <ufo3764@naver.com>
2022-06-30 15:35:16 +0900

    add: new file for listview

---
+++ app/src/main/java/com/example/myapplication/ui/home/MyAdapter.java
@@ -0,0 +1,53 @@
+package com.example.myapplication.ui.home;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.myapplication.R;
+
+import java.util.ArrayList;
+
+public class MyAdapter extends BaseAdapter {
+    Context mContext = null;
+    LayoutInflater mLayoutInflater = null;
+    ArrayList<Data> sample;
+
+    public MyAdapter(Context mContext, ArrayList<Data> sample) {
+        this.mContext = mContext;
+        this.mLayoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.sample = sample;
+    }
+
+    @Override
+    public int getCount() {
+        return sample.size();
+    }
+
+    @Override
+    public Data getItem(int position) {
+        return sample.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if(convertView == null)
+            convertView = mLayoutInflater.inflate(R.layout.list_view, null);
+
+        TextView nameView = (TextView) convertView.findViewById(R.id.name);
+        TextView phoneView = (TextView) convertView.findViewById(R.id.phone);
+
+        nameView.setText(sample.get(position).getName());
+        phoneView.setText(sample.get(position).getPhone());
+
+        return convertView;
+    }
+}
